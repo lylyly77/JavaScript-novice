27f6706dfd5f219bb0b3f7bc1128e39e95b857c3
LYLYLY <lylyly72020@163.com>
2023-06-29 21:57:38 +0800

    数组的基本使用

---
+++ 03-javscript输入输出/src/main.js
@@ -0,0 +1,12 @@
+
+// JavaScript的输出
+alert("你好 JS~")  // 页面警示框
+document.write("JavaScript 我来了")  //body 标签展示
+console.log("它~会魔法吧~")  //控制台展示
+
+
+// JavaScript的输入
+window.prompt("请输入你的用户名") // 页面输入警示框
+// JavaScript代码执行顺序
+// 按HTML文档流顺序执行JavaScript代码
+// 但是遇到alert()和prompt()会跳过页面渲染先被执行
